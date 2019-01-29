dd432c5b6b74677075e28d5b3559d57fcb0195cb
bfan <bfan@ubiquant.com>
2019-01-29 13:47:35 +0800

    added grpc

--- client/client.py
+++ client/client.py
@@ -1,35 +1,72 @@
+import sys
+VERSION=''
+with open('/inc/version.inc') as f:
+    lines = f.readlines()
+    VERSION = lines[0].split('=')[1].strip()
+sys.path.append('/opt/version/'+VERSION+'/python-libs')
 import threading
-
 import grpc
-import communicate.dealer_pb2 as dealer
+import sys
+from  pathlib import Path
+_current_root = str(Path(__file__).resolve().parents[1])
+sys.path.append(_current_root)
+import communicate.dealer_pb2 as dealer_pb2
 import communicate.dealer_pb2_grpc as rpc
+from ubiqtool.thread_jobs import Job
+import time
 
 address = 'localhost'
 port = 11912
 
 
 class Client(object):
-
-    def __init__(self, u: str):
+    def __init__(self, u: str, AI):
         # the frame to put ui components on
         self.username = u
         # create a gRPC channel + stub
         channel = grpc.insecure_channel(address + ':' + str(port))
+        # self.request_iterator = ClientRequester()
         self.conn = rpc.GameStub(channel)
+        self.ai = AI
+        self._lock = threading.Lock()
+        self._response_so_far = []
+        self._request_so_far = []
+        self._new_response = []
+        self._new_request = []
 
-    def send_message(self, event):
+    def chat_with_server(self):
+        while True:
+            if len(self._new_request) != 0:
+                # self._lock.acquire()
+                msg = self._new_request.pop(0)
+                # self._lock.release()
+                print(msg)
+                yield msg
+
+    def start(self):
         """
-        This method is called when user enters something into the textbox
         """
-        message = self.entry_message.get()
-        if message is not '':
-            n = dealer.Note()
-            n.name = self.username
-            n.message = message
-            print("S[{}] {}".format(n.name, n.message))
-            self.conn.SendNote(n)
+        responses = self.conn.GameStream(self.chat_with_server())
+        for res in responses:
+            print(res)
+
+    def run(self):
+        while True:
+            self.add_request(Client.HeartBeat())
+            time.sleep(1)
+
+    def add_request(self, msg):
+        self._lock.acquire()
+        self._new_request.append(msg)
+        self._lock.release()
+
+    @staticmethod
+    def HeartBeat():
+        return dealer_pb2.DealerRequest(command='heartbeat')
 
 
 if __name__ == '__main__':
     username = 'bfan'
-    c = Client(username)
+    c = Client(username, None)
+    Job(c).start()
+    c.start()
--- server.py
+++ server.py
@@ -3,18 +3,21 @@ from concurrent import futures
 import grpc
 import time
 
-import communicate.dealer_pb2 as dealer
+import communicate.dealer_pb2 as dealer_pb2
 import communicate.dealer_pb2_grpc as rpc
 
 
-class ChatServer(rpc.GameServicer):
+class GameServer(rpc.GameServicer):
 
-    def __init__(self):
+    def __init__(self, state):
         # List with all the chat history
-        self.chats = []
+        self.state = state
+
+        self.request = []
+        self.response = []
 
     # The stream which will be used to send new messages to clients
-    def ChatStream(self, request_iterator, context):
+    def GameStream(self, request_iterator, context):
         """
         This is a response-stream type call. This means the server can keep sending messages
         Every client opens this connection and waits for server to send new messages
@@ -27,21 +30,32 @@ class ChatServer(rpc.GameServicer):
         # For every client a infinite loop starts (in gRPC's own managed thread)
         while True:
             # Check if there are any new messages
-            while len(self.chats) > lastindex:
-                n = self.chats[lastindex]
-                lastindex += 1
-                yield n
+            for item in request_iterator:
+                self.request.append(item)
+                while len(self.response) != 0:
+                    yield self.response.pop()
+
+    def run(self):
+        while True:
+            if len(self.request) != 0:
+                request = self.request.pop()
+                self.response.append(GameServer.void_reply())
+            time.sleep(0.1)
+
+    @staticmethod
+    def void_reply():
+        return dealer_pb2.DealerReply(message='void')
 
 
 if __name__ == '__main__':
     port = 11912
     # create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    rpc.add_GameServicer_to_server(ChatServer(), server)
+    game_server = GameServer(None)
+    rpc.add_GameServicer_to_server(game_server, server)
 
-    print('Starting server. Listening...')
+    print('Starting server. Listening...', port)
     server.add_insecure_port('[::]:' + str(port))
     server.start()
     # Server starts in background (another thread) so keep waiting
-    while True:
-        time.sleep(64 * 64 * 100)
+    game_server.run()
